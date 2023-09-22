3e50855863463449738443a6f17c4aaec9026e40
TheUnsocialEngineer <theunsocialengineer@protonmail.com>
2023-09-23 00:44:04 +0100

    0.76.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Antiland",
-    version="0.74",
+    version="0.76.5",
     packages=find_packages(),
     install_requires=[
         "requests",
