613dd66cc24456dd9fc3cb3213c6abf15efa7c09
TheUnsocialEngineer <TheUnsocialEngineer@protonmail.com>
2023-10-11 15:22:00 +0100

    new conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('/Antiland'))
+
 project = 'Antiland'
 copyright = '2023, TheUnsocialEngineer'
 author = 'TheUnsocialEngineer'
