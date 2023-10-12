c83cd5e67e1927a52a52ef468b810529f2e45f7a
TheUnsocialEngineer <TheUnsocialEngineer@protonmail.com>
2023-10-12 12:34:39 +0100

    also switched to property

--- Antiland/account.py
+++ Antiland/account.py
@@ -1,41 +1,159 @@
 class Account:
     def __init__(self, data):
-        self.objectId = data.get("objectId")
-        self.lastOpen = data.get("lastOpen")
-        self.userLangs = data.get("userLangs")
-        self.username = data.get("username")
-        self.country = data.get("country")
-        self.lang = data.get("lang")
-        self.avatar = data.get("avatar")
-        self.likesMale = data.get("likesMale")
-        self.likesFemale = data.get("likesFemale")
-        self.color = data.get("color")
-        self.antiKarma = data.get("antiKarma")
-        self.rating = data.get("rating")
-        self.msgCount = data.get("msgCount")
-        self.pvtcCount = data.get("pvtcCount")
-        self.age = data.get("age")
-        self.search = data.get("search")
-        self.createdAt = data.get("createdAt")
-        self.updatedAt = data.get("updatedAt")
-        self.quest = data.get("quest")
-        self.profileName = data.get("profileName")
-        self.pvtChannelId = data.get("pvtChannelId")
-        self.dOk = data.get("dOk")
-        self.blockedBy = data.get("blockedBy")
-        self.totalBans = data.get("totalBans")
-        self.more = data.get("more")
-        self.female = data.get("female")
-        self.minKarma = data.get("minKarma")
-        self.acceptRandoms = data.get("acceptRandoms")
-        self.lastChangeDate = data.get("lastChangeDate")
-        self.email = data.get("email")
-        self.emailIsVerified = data.get("emailIsVerified")
-        self.artifacts = data.get("artifacts")
-        self.lastAction = data.get("lastAction")
-        self.authData = data.get("authData")
-        self.emailIsValid = data.get("emailIsValid")
-        self.ACL = data.get("ACL")
-        self.__type = data.get("__type")
-        self.className = data.get("className")
-        self.sessionToken = data.get("sessionToken")
+        self._data = data
+
+    @property
+    def objectId(self):
+        return self._data.get("objectId")
+
+    @property
+    def lastOpen(self):
+        return self._data.get("lastOpen")
+
+    @property
+    def userLangs(self):
+        return self._data.get("userLangs")
+
+    @property
+    def username(self):
+        return self._data.get("username")
+
+    @property
+    def country(self):
+        return self._data.get("country")
+
+    @property
+    def lang(self):
+        return self._data.get("lang")
+
+    @property
+    def avatar(self):
+        return self._data.get("avatar")
+
+    @property
+    def likesMale(self):
+        return self._data.get("likesMale")
+
+    @property
+    def likesFemale(self):
+        return self._data.get("likesFemale")
+
+    @property
+    def color(self):
+        return self._data.get("color")
+
+    @property
+    def antiKarma(self):
+        return self._data.get("antiKarma")
+
+    @property
+    def rating(self):
+        return self._data.get("rating")
+
+    @property
+    def msgCount(self):
+        return self._data.get("msgCount")
+
+    @property
+    def pvtcCount(self):
+        return self._data.get("pvtcCount")
+
+    @property
+    def age(self):
+        return self._data.get("age")
+
+    @property
+    def search(self):
+        return self._data.get("search")
+
+    @property
+    def createdAt(self):
+        return self._data.get("createdAt")
+
+    @property
+    def updatedAt(self):
+        return self._data.get("updatedAt")
+
+    @property
+    def quest(self):
+        return self._data.get("quest")
+
+    @property
+    def profileName(self):
+        return self._data.get("profileName")
+
+    @property
+    def pvtChannelId(self):
+        return self._data.get("pvtChannelId")
+
+    @property
+    def dOk(self):
+        return self._data.get("dOk")
+
+    @property
+    def blockedBy(self):
+        return self._data.get("blockedBy")
+
+    @property
+    def totalBans(self):
+        return self._data.get("totalBans")
+
+    @property
+    def more(self):
+        return self._data.get("more")
+
+    @property
+    def female(self):
+        return self._data.get("female")
+
+    @property
+    def minKarma(self):
+        return self._data.get("minKarma")
+
+    @property
+    def acceptRandoms(self):
+        return self._data.get("acceptRandoms")
+
+    @property
+    def lastChangeDate(self):
+        return self._data.get("lastChangeDate")
+
+    @property
+    def email(self):
+        return self._data.get("email")
+
+    @property
+    def emailIsVerified(self):
+        return self._data.get("emailIsVerified")
+
+    @property
+    def artifacts(self):
+        return self._data.get("artifacts")
+
+    @property
+    def lastAction(self):
+        return self._data.get("lastAction")
+
+    @property
+    def authData(self):
+        return self._data.get("authData")
+
+    @property
+    def emailIsValid(self):
+        return self._data.get("emailIsValid")
+
+    @property
+    def ACL(self):
+        return self._data.get("ACL")
+
+    @property
+    def __type(self):
+        return self._data.get("__type")
+
+    @property
+    def className(self):
+        return self._data.get("className")
+
+    @property
+    def sessionToken(self):
+        return self._data.get("sessionToken")
