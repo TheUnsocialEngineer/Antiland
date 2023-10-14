a50bcc96eb9e7bd55308c0cfdcc97a17aee0aa88
TheUnsocialEngineer <theunsocialengineer@protonmail.com>
2023-10-14 19:51:00 +0100

    smh

---
+++ docs/conf.py
@@ -0,0 +1,74 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('C:/Users/JordansGamingRig/Desktop/Antiland'))
+sys.path.append(os.path.abspath(
+    os.path.join(__file__, "../../Antiland")
+))
+sys.path.append(os.path.abspath('../extensions'))
+
+autodoc_mock_imports = ["aiohttp"]
+
+# -- Project information -----------------------------------------------------
+
+project = 'Antiland'
+copyright = '2023, TheUnsocialEngineer'
+author = 'TheUnsocialEngineer'
+
+# The full version, including alpha/beta/rc tags
+release = '0.93'
+<<<<<<< HEAD
+=======
+
+>>>>>>> bca6481ea8588bba3c5955ef0a60f54633a99c32
+
+autodoc_member_order = 'groupwise'
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'jsoner',
+    'attributetable',
+    'sphinx_rtd_theme'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+html_favicon = 'antiland_logo.ico'
+
+import sys; sys.setrecursionlimit(5000)
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
