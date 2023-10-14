7b371d2a58901bedd8fc4633c8f4fa7fcaf05e9b
TheUnsocialEngineer <theunsocialengineer@protonmail.com>
2023-10-14 20:08:23 +0100

    back to classic it is then i guess

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,6 @@ extensions = [
     'sphinx_rtd_theme'
 ]
 
-os.popen("pip3 install sphinx_rtd_theme")
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 html_favicon = 'antiland_logo.ico'
@@ -63,7 +61,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
