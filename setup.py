ca9dd5691f565ffd770b0583c58249fa15996a47
TheUnsocialEngineer <theunsocialengineer@protonmail.com>
2023-10-14 19:48:07 +0100

    0.94

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Antiland",
-    version="0.93",
+    version="0.94",
     packages=find_packages(),
     install_requires=[
         "json",
